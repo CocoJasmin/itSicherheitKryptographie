29fb943f46202402871a9c882de66b0bcebb4b0b
coco <cosima.dotzauer@googlemail.com>
2022-03-02 01:16:47 +0100

    good, it's one a clock, I'm a little bit tired. Looking forward for breakfast tomorrow! But it's going to be so early... Should I shower now or in seven hours?....Questions about questions. By the way S01 and S02 are complete now. (I guess...)

--- src/main/java/S01_ConsoleClasses/ReportJarConfiguration.java
+++ src/main/java/S01_ConsoleClasses/ReportJarConfiguration.java
@@ -4,7 +4,6 @@ public enum ReportJarConfiguration {
     instance;
 
     public final String fileSeparator = System.getProperty("file.separator");
-    public final String userDirectory = System.getProperty("user.dir");
 
     public final String nameOfJavaArchive = "report.jar";
     public final String nameOfClass = "Report";
@@ -12,6 +11,5 @@ public enum ReportJarConfiguration {
 
     public final String nameOfSubFolder = "report" + fileSeparator + "jar";
     public final String subFolderPathOfJavaArchive = nameOfSubFolder + fileSeparator + nameOfJavaArchive;
-    public final String fullPathToJavaArchive = userDirectory + fileSeparator + subFolderPathOfJavaArchive;
 
 }
